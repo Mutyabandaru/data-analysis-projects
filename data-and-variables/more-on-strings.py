23b99c9d7f65a0d0e4494dcf2874a30af194743b
mutya bandaru <mutya.bandaru@gmail.com>
2025-10-30 23:46:59 -0400

    Data and variable Excercise

--- data-and-variables/more-on-strings.py
+++ data-and-variables/more-on-strings.py
@@ -1,2 +1,107 @@
-print(type("17"))
-print(type("3.2"))
+# print(type("17"))
+# print(type("3.2"))
+
+# print(type(int("18")))
+
+# print(type('This is a string'))
+# print(type("And so is this"))
+
+# print("Bruce's beard")
+
+# print(42000)
+# print(42,000)
+
+# print(42, 17, 56, 34, 11, 4.35, 32)
+# print(3.4, "hello", 45)
+
+# print(int("2345"))
+# print(type(int("2345")))
+# print(int(17))
+# print(17)
+
+# print(type(int('12char')))
+
+# print(str(17))
+# print(str(123.45))
+# print(type(str(123.45)))
+
+# print(type(int('3'))) 
+# print(type(int('three'))) 
+# print(type(int('3 3')))
+# print(type(int('33'))) 
+
+# message = "hello world"
+# print (message)
+
+# message = "Python ROCKS!"
+# num = 17
+# pi = 3.14159
+
+# print(type((message)))
+
+# print(type(5))
+# print(type(5.0))
+# print(type("5"))
+
+
+# print(5 * 2)
+# print(5 ** 2)
+# print("5" * 2)
+
+# budget = '1000'
+# budget = int(budget)
+# print(budget * 5)
+
+# print("Data" + "Science")
+# print(5 + 5)
+
+# salary = 8000
+# print("salary:" + str(salary))
+
+
+# year = 2025
+# month = 10
+# print("Date:", year, "-", str(month))
+
+
+# revenue = 12000
+# expenses = 8000
+# profit = revenue - expenses
+# print(f"Banana stand profit: ${profit}")
+
+# revenue = 13500
+# print(revenue)
+# print(f"Banana stand profit: ${revenue - expenses}")
+
+# print(10 % 3)
+
+# day_number = 23
+# print(day_number % 7)
+
+# sales = 48000
+# costs = 31500
+# profit = sales - costs
+# profit_margin=(profit/sales)*100
+# print(f"profit margin: {profit_margin}%")
+
+# radius_of_circle = 5
+# PI = 3.14
+# area_of_circle = PI * radius_of_circle ** 2
+# print(f"area of circle : ${area_of_circle}")
+
+# minutes = 645
+# hours = minutes / 60
+# print(hours)
+
+# print (12 % 4 )
+# print (13 % 7)
+# print (6 % 2 )
+# print (7 % 2 )
+# print(16 - 2 * 5 / 3 + 1)
+# print(1 + 5 % 3)
+# print(2 ** 2 ** 3 * 3)
+
+# print("Launch" + "Code" )
+# print(1+1)
+# print(1 + "1")
+
