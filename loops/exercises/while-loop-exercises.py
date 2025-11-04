811b5c3baa5fe9805fafe0ee6738920607e90117
mutya bandaru <mutya.bandaru@gmail.com>
2025-11-03 22:12:27 -0500

    Loops

--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -1,25 +1,35 @@
 # Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
-
-
-
+Starting_fuel_level = 0
+Number_of_astronauts_aboard = 0
+The_altitude_the_shuttle_reaches = 0
 
 
 # Exercise #1: Construct while loops to do the following:
   # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
-
-
+while Starting_fuel_level <= 5000 or Starting_fuel_level >30000:
+    Starting_fuel_level = int(input("Enter the starting fuel level: "))
+  
+      
 
 
 
 # b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+while Number_of_astronauts_aboard < 1 or Number_of_astronauts_aboard > 7:
+    Number_of_astronauts_aboard = int(input("Enter the number of astronaut: "))
+
   
   
   
 # c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
+while Starting_fuel_level -100*Number_of_astronauts_aboard >= 0:
+   The_altitude_the_shuttle_reaches += 50
+   Starting_fuel_level -= 100*Number_of_astronauts_aboard
 
 # Exercise #2: Print the result with the phrase, The shuttle gained an altitude of ___ km and has ___ kg of fuel left. Fill in the blanks with the altitude and fuel level values.
-
+# print(f"The shuttle gained an altitude of {The_altitude_the_shuttle_reaches} km and has {Starting_fuel_level} kg of fuel left.")
 # If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
+if The_altitude_the_shuttle_reaches >= 2000:
+    ending =  "Orbit achieved!"
+else:
+     ending =  "Failed to reach orbit"
+print("The shuttle gained an altitude of" , The_altitude_the_shuttle_reaches, "km and has", Starting_fuel_level, "kg of fuel left." ,ending)
